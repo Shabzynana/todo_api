0df50f4cd111a2e9379ea570581ade127acc7597
MAC <horlarmihleykan10@gmail.com>
2024-03-28 01:35:45 +0100

    new changes

---
+++ todo_api/config.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv(".env")
+
+
+# pylint: disable=invalid-name
+class App_Config:
+    """_summary_"""
+
+    SECRET_KEY = os.getenv("SECRET_KEY", "test")
+
+    # SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
+
+
+    # SESSION CONFIG
+    SESSION_TYPE = "filesystem" if not os.getenv("PROD", None) else "sqlalchemy"
+    # SESSION_USE_SIGNER = True
+    # SESSION_COOKIE_SECURE = False
+    # SESSION_COOKIE_SAMESITE = "None"
+    # SESSION_COOKIE_HTTPONLY = False
+    # PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=30)
+
+    ## MAIL CONFIG
+    # MAIL_SERVER = 'sandbox.smtp.mailtrap.io'
+    # MAIL_PORT = 2525
+    # MAIL_USE_TLS = True
+    # MAIL_USE_SSL = False
+    # MAIL_USERNAME = os.getenv("USER_MAIL")
+    # MAIL_PASSWORD = os.getenv("PASSWORD_MAIL")
+
+
+
+
+
+
+
+
+
+
+
---
+++ todo_api/init.py
@@ -0,0 +1,60 @@
+from flask import Flask, session
+from flask_session import Session  # type: ignore
+
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_mail import Mail
+from flask_bcrypt import Bcrypt
+from todo_api.config import App_Config
+
+
+db = SQLAlchemy()
+
+bcrypt = Bcrypt()
+
+
+sess = Session()
+
+mail = Mail()
+
+
+def create_app():
+    """
+    Create a new instance of the app with the given configuration.
+
+    :param config_class: configuration class
+    :return: app
+    """
+    # Initialize Flask-
+
+    app = Flask(__name__)
+    app.config.from_object(App_Config)
+#     if app.config["SQLALCHEMY_DATABASE_URI"]:
+#         print(f"using db")
+
+    # Initialize SQLAlchemy
+    db.init_app(app)
+
+    # initialise seesion
+    sess.init_app(app)
+
+    # Initialize Bcrypt
+    bcrypt.init_app(app)
+
+    # Initialize Bcrypt
+    mail.init_app(app)
+
+    # Importing the models here so it can create the empty tables.
+    # the routes
+
+    # from todo_app.users.views import users
+    # from todo_app.todo.views import todos
+
+    # app.register_blueprint(users)
+    # app.register_blueprint(todos)
+
+    # create db tables from models if not exists
+    with app.app_context():
+        db.create_all()
+
+    return app
