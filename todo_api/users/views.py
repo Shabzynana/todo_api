1ae5deffa1e027c08cd9e3eaa92927b40dd6ed84
MAC <horlarmihleykan10@gmail.com>
2024-04-17 16:49:37 +0100

    registration function

--- todo_api/users/views.py
+++ todo_api/users/views.py
@@ -1,6 +1,6 @@
 # import datetime
-from flask import url_for,flash,request,redirect,Blueprint,abort,jsonify
-from todo_api.utils.serializers import user_schema, users_schema
+from flask import url_for,flash,request,redirect,Blueprint,abort,jsonify,session  #type : ignore
+from todo_api.utils.serializers import user_schema, users_schema,User_Schema
 from todo_api import db,bcrypt
 from todo_api.models import User, Todo
 # from todo_app.users.picture import current_user_id, login_required, check_confirmed
@@ -9,67 +9,120 @@ from todo_api.models import User, Todo
 users = Blueprint('users',__name__,url_prefix="/api")
 
 
-@users.route('/register', methods=['GET','POST'])
-def register():
 
-    data = request.get_json() if request.get_json() != None else request.form
+@users.route('/home', methods=['GET'])
+def home():
+
+    return jsonify({"message": "i love jesus"})
+
+@users.route("/users", methods=['GET'])    
+def all_users():
 
     try:
-        data = user_schema(**data)
+        users = User.query.all()
+        if users:
+            result = users_schema.dump(users)
+            return {"msg": result}
+        return {"msg": "No user yet"} 
+    except:
+        pass     
 
-        email_exists = User.query.filter_by(email=data.email).first()
+
+         
+
+
+@users.route('/register', methods=['POST'])
+def register():
+
+    # data = request.get_json() if request.get_json() != None else request.form
+
+    # try:
+    #     data = User_Schema(**data)
+
+    #     email_exists = User.query.filter_by(email=data.email).first()
+    #     if email_exists:
+    #         return (
+    #             jsonify({"error": "Forbbiden", "message": "Email already exists!"}),
+    #             403,
+    #         )
+
+    #     hashed_password = bcrypt.generate_password_hash(data.password).decode("utf-8")
+
+    #     new_user = User(email=data.email, first_name=data.first_name, last_name=data.last_name, username=data.username, gender=data.gender, password=hashed_password)
+    #     db.session.add(new_user)
+    #     db.sesssion.commit()
+
+    #     return (
+    #         jsonify(
+    #             {
+    #                 "message": "User Created Succesfully",
+    #                 "data": user_schema.dump(new_user),
+    #             }
+    #         ),
+    #         201,
+    #     )
+    # except:
+    #     return jsonify({"message":"failed"})    
+    # except Exception as error:
+    #     error_message = str(error)  # Convert the error to a string
+    #     print(f"{type(error).__name__}: {error}")
+    #     return (
+    #         jsonify(
+    #             {
+    #                 "error": "failed",
+    #                 "message": error_message
+    #                 # "message": "Internal Error: User not created",
+    #             }
+    #         ),
+    #         500,
+    #     )
+    
+
+    try:
+
+        first_name = request.json['first_name']
+        last_name = request.json['last_name']
+        username = request.json['username']
+        email = request.json['email']
+        gender = request.json['gender']
+        password = request.json['password']
+
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        email_exists = User.query.filter_by(email=email).first()
         if email_exists:
             return (
                 jsonify({"error": "Forbbiden", "message": "Email already exists!"}),
                 403,
             )
 
-        hashed_password = bcrypt.generate_password_hash(data.password).decode("utf-8")
 
-        new_user = User(data.name, data.email, hashed_password)
-        new_user.insert()
+        new_user = User(first_name=first_name, last_name=last_name,username=username,email=email,gender=gender,password=hashed_password,confirmed=False)
+        db.session.add(new_user)
+        db.session.commit()
 
-        # session["user"] = {"id": new_user.id}
+        print(new_user)
+        result = user_schema.dump(new_user)
+        return jsonify({
+            'msg': "User Created",
+            'user': result }), 201
 
+
+    except Exception as error:
+        error_message = str(error)  # Convert the error to a string
+        print(f"{type(error).__name__}: {error}")
         return (
             jsonify(
                 {
-                    "message": "User Created Succesfully",
-                    "data": new_user.format(),
+                    "error": "failed",
+                    "message": error_message
+                    "message": "Internal Error: User not created",
                 }
             ),
-            201,
-        )
-    except:
-        pass    
+            500,
+    )              
 
-    # first_name = request.json['first_name']
-    # last_name = request.json['last_name']
-    # username = request.json['username']
-    # email = request.json['email']
-    # gender = request.json['gender']
-    # password = request.json['password']
 
-    # hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-
-    # new_user = User(first_name,last_name,username,email,gender,hashed_password)
-    # db.session.add(new_user)
-    # db.session.commit()
-
-    # print(new_user)
-    # result = user_schema.dump(new_user)
-    # return jsonify({
-    #     'msg': "User Created",
-    #     'user': result }), 201
-
-# @users.route("/api/users", methods=['GET'])    
-# def all_users():
-
-#     users = User.query.all()
-#     if users:
-#         result = users_schema.dump(users)
-#         return {"msg": result}
-#     return {"msg": "No user yet"}       
 
 # @users.route("/api/logout", methods=['POST'])
 # def logout():
@@ -77,19 +130,20 @@ def register():
 #     return {"msg": "User logout, Successful"}
 
 
-# @users.route('/api/login', methods=['GET','POST'])
-# def login():
+@users.route('/api/login', methods=['GET','POST'])
+def login():
 
-#     email = request.json['email']
-#     password = request.json['password']
+    email = request.json['email']
+    password = request.json['password']
 
-#     user = User.query.filter_by(email=email).first()
-#     if user is None:
-#         return {"msg": "User with email not found"}, 404
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        return {"msg": "User with email not found"}, 404
 
-#     elif bcrypt.check_password_hash(user.password, password) and user is not None:
-#         login_user(user)
-#         return {"msg": "login Successful"}, 200
+    elif bcrypt.check_password_hash(user.password, password) and user is not None:
+        session['logged_in'] = True
+        session["user_id"] = {"id": user.id}
+        return {"msg": "login Successful"}, 200
 
-#     elif bcrypt.check_password_hash(user.password, password) is None or user is not None:
-#         return {"msg": "Incorrect password"}, 400
+    elif bcrypt.check_password_hash(user.password, password) is None or user is not None:
+        return {"msg": "Incorrect password"}, 400
